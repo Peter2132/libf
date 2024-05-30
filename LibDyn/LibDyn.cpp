c555c74680f6ec6a98de2db080d206f6e2054b99
Сергей Трофимов <isip_p.s.trofimov@mpt.ru>
2024-05-30 14:43:17 +0300

    libt

---
+++ LibDyn/LibDyn.cpp
@@ -0,0 +1,43 @@
+﻿#include <windows.h>
+#include <string>
+#include <sstream>
+#include <set>
+#include <algorithm>
+#include <cctype>
+
+extern "C" __declspec(dllexport) bool Search(const char* sentence, const char* wordsToFind) {
+   
+    std::string lowerSentence(sentence);
+    std::transform(lowerSentence.begin(), lowerSentence.end(), lowerSentence.begin(), ::tolower);
+
+    std::string lowerWordsToFind(wordsToFind);
+    std::transform(lowerWordsToFind.begin(), lowerWordsToFind.end(), lowerWordsToFind.begin(), ::tolower);
+
+    
+    std::set<std::string> Searchword;
+    std::string word;
+    std::istringstream iss(lowerWordsToFind);
+    while (std::getline(iss, word, ' ')) {
+        Searchword.insert(word);
+    }
+
+    for (const auto& word : Searchword) {
+        if (lowerSentence.find(word) == std::string::npos) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
---
+++ Prilozenie/Prilozenie.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <set>
+#include <algorithm>
+#include <cctype>
+
+
+
+
+int main() {
+    SetConsoleCP(1251);
+    setlocale(LC_ALL, "Russian");
+
+    HINSTANCE hGetProcIDDLL = LoadLibraryA("LibDyn.dll");
+
+    if (hGetProcIDDLL == nullptr) {
+        std::cout << "Could not load the dynamic library" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    typedef bool(*Searchwords)(const char*, const char*);
+    Searchwords checkWordsAndSentence =
+        (Searchwords)GetProcAddress(hGetProcIDDLL, "Searchwords");
+
+    if (!checkWordsAndSentence) {
+        std::cout << "Could not locate the function" << std::endl;
+        FreeLibrary(hGetProcIDDLL);
+        return EXIT_FAILURE;
+    }
+
+    std::string sentence, forwords;
+    std::cout << "Введите слово или предложение: ";
+    std::getline(std::cin, sentence);
+    std::cout << "Введите слово или букву, которую хотите найти: ";
+    std::getline(std::cin, forwords);
+
+    bool result = checkWordsAndSentence(sentence.c_str(), forwords.c_str());
+    std::cout << "Результат: " << (result ? "есть!" : "нету") << std::endl;
+
+    FreeLibrary(hGetProcIDDLL);
+
+    return EXIT_SUCCESS;
+}
+
